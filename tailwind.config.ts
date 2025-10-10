19dc691395fde4eb6ab570e282762e97d70f6404
Aswin Krishna <ltaswinak@gmail.com>
2025-10-10 09:04:06 +0000

    change all the font to Nunito Sans

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['"Nunito Sans"', 'sans-serif'],
-        headline: ['"Lora"', 'serif'],
+        headline: ['"Nunito Sans"', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
