32ebc5ada8c965024f6db9b7c889468009dc7bb3
Aswin Krishna <ltaswinak@gmail.com>
2025-10-16 20:43:02 +0000

    make sure only after login we can access the other pages

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,10 @@
 
 import { withAuth } from "next-auth/middleware"
-import { NextResponse } from "next/server"
 
 export default withAuth(
+  // `withAuth` augments your `Request` with the user's token.
   function middleware(req) {
-    return NextResponse.next()
+    // You can add custom logic here if needed, otherwise just let it pass through.
   },
   {
     callbacks: {
